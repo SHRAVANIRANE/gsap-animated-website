3ecbf3ac0350361759452e97e7e2f6477c2aab95
Shravani Rane <raneshravani21@gmail.com>
2025-06-28 00:46:34 +0530

    Worked on Hero Section

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+  return <button id={id} className="group relative" />;
+};
+
+export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        general: ["general", "sans-serif"],
+        circular: ["circular-web", "sans-serif"],
+        robertMedium: ["robert-medium", "sans-serif"],
+        robertRegular: ["robert-regular", "sans-serif"],
+        zentry: ["Zentry", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
