f3c4c2135502af6a5c3d118daf7a09f37b391ef8
Shravani Rane <raneshravani21@gmail.com>
2025-06-28 15:30:31 +0530

    Designed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className="relative min-h-screen w-screen overflow-x-hidden"></section>
     </main>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
-  return <button id={id} className="group relative" />;
+  return (
+    <button
+      id={id}
+      className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full ng-voilet-50 px-7 py-3 text-black"
+      ${containerClass}`}
+    >
+      {leftIcon}
+      <span className="realtive incline-flex overflow-hidden font-general rexr-xs uppercase">
+        <div>{title}</div>
+        {rightIcon}
+      </span>
+    </button>
+  );
 };
 
 export default Button;
