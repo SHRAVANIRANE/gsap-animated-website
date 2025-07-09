42d4acab6a48dfc799e26b135a10fb56b74da87a
Shravani Rane <raneshravani21@gmail.com>
2025-07-09 13:43:54 +0530

    Completed Navbar Navigation

---
+++ src/components/Features.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <div>
+      <section className="bg-black pb-52">
+        <div className="container mx-auto px-3 md:px-10">
+          <div className="px-5 py-32">
+            <p>Into the Metagame Layer</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Features;
